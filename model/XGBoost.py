6ef876d2c25206dd0e37a36354714c1090173c28
cleverbobo <977516585@qq.com>
2023-01-09 19:58:30 +0800

    XGBoost调参

---
+++ model/XGBoost.py
@@ -0,0 +1,81 @@
+import sys
+import argparse
+sys.path.append("./")
+
+import joblib
+from sklearn.multioutput import MultiOutputRegressor
+import xgboost  as xgb
+import pandas as pd
+
+from utils import Processing_data
+
+# XGBoost训练函数
+def TrainXGBoost(dataset_path, input_seq_length, output_size):
+
+    # 加载数据集
+    train_X, train_y, test_X, test_y, data_x, dataset = Processing_data(dataset_path, input_seq_length, output_size,model_type = 'XGBoost')
+    
+    # 参数设定
+    other_params = {
+        'learning_rate': 0.1, 
+        'n_estimators': 300, 
+        'max_depth': 10, 
+        'min_child_weight': 1,
+        'seed': 0, 
+        'subsample': 0.8, 
+        'colsample_bytree': 0.8, 
+        'gamma': 0, 
+        'reg_alpha': 0, 
+        'reg_lambda': 1
+    }
+
+    # XGBoost训练
+    model = MultiOutputRegressor(xgb.XGBRegressor(objective='reg:squarederror',**other_params))
+    model.fit(train_X, train_y)
+
+    # 保存模型
+    joblib.dump(model, "./checkpoints/XGBoost.pkl")
+
+    # 训练集结果
+    train_loss = train_y-model.predict(train_X)
+    data = pd.DataFrame(train_loss)
+    data.to_excel("train_loss.xlsx")
+
+    # 测试集结果
+    test_loss = test_y-model.predict(test_X)
+    data = pd.DataFrame(test_loss)
+    data.to_excel("test_loss.xlsx")
+
+# 测试端口
+def TestXGBoost(dataset_path, input_seq_length, output_size):
+    model = joblib.load("./checkpoints/XGBoost.pkl")
+
+    # 加载数据集
+    train_X, train_y, test_X, test_y, data_x, dataset = Processing_data(dataset_path, input_seq_length, output_size,model_type = 'XGBoost')
+
+    # 训练集结果
+    train_loss = train_y-model.predict(train_X)
+    data = pd.DataFrame(train_loss)
+    data.to_excel("train_loss.xlsx")
+
+    # 测试集结果
+    test_loss = test_y-model.predict(test_X)
+    data = pd.DataFrame(test_loss)
+    data.to_excel("test_loss.xlsx")
+    
+
+
+if __name__ == '__main__':
+    # 训练函数
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--DatasetPath', type=str, default='./dataset/real_data.csv', help='数据集路径')
+    parser.add_argument('--InputSeqLength', type=int, default=288, help='输入序列的长度')
+    parser.add_argument('--OutputSize', type=int, default=1,help='输出尺寸')
+    opt = parser.parse_args()
+
+    dataset_path = opt.DatasetPath
+    input_seq_length = opt.InputSeqLength
+    output_size = opt.OutputSize
+    
+    TestXGBoost(dataset_path, input_seq_length, output_size)
+    # TrainXGBoost(dataset_path, input_seq_length, output_size)
